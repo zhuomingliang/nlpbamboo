e563f852df3681fa2485a27d720ade6264b2ef78
detrox <detrox@22020200-94fc-11dd-9691-531050c25895>
2008-09-04 08:19:11 +0000

    added ascii processor

---
+++ source/processor/ascii_processor.cxx
@@ -0,0 +1,54 @@
+#include <ctype.h>
+#include <iostream>
+
+#include "utf8.hxx"
+#include "lex_token.hxx"
+#include "ascii_processor.hxx"
+
+AsciiProcessor::AsciiProcessor(IConfig *config)
+{
+	config->get_value("chinese_number_end", _chinese_number_end);
+}
+
+void AsciiProcessor::_process(LexToken *token, std::vector<LexToken *> &out)
+{
+	const char *s = token->get_token();
+	char first[8], *subtoken, *subtoken_ptr;
+	size_t len;
+	enum {
+		state_unknow = 0,
+		state_alpha,
+		state_number,
+		state_end
+	} state, last;
+
+	if (*s == '\0') return;
+	subtoken = new char [strlen(s) + 1];
+	subtoken[0] = '\0';
+	subtoken_ptr = subtoken;
+	state = last = state_unknow;
+	while (true) {
+		len = utf8::first(s, first);
+		if (isalpha(*first)) state = state_alpha;
+		else if (isdigit(*first)) state = state_number;
+		else if (*first == '.' && last == state_number) state = state_number;
+		else if (strstr(_chinese_number_end, first) && last == state_number) state = state_number;
+		else if (*first == '\0') state = state_end;
+		else state = state_unknow;
+		if (last != state && subtoken[0]) {
+			LexToken::attr_t attr = LexToken::attr_unknow;
+			if (last == state_alpha) attr = LexToken::attr_alpha;
+			else if (last == state_number) attr = LexToken::attr_number;
+			*subtoken_ptr = '\0';
+			out.push_back(new LexToken(subtoken, attr));
+			subtoken_ptr = subtoken;
+			subtoken[0] = '\0';
+			if (state == state_end) break;
+		}
+		memcpy(subtoken_ptr, first, len);
+		subtoken_ptr += len;
+		s += len;
+		last = state;
+	}
+	delete []subtoken;
+}
---
+++ source/processor/ascii_processor.hxx
@@ -0,0 +1,21 @@
+#ifndef ASCII_PROCESSOR_HXX
+#define ASCII_PROCESSOR_HXX
+
+#include "processor.hxx"
+#include "ilexicon.hxx"
+
+class AsciiProcessor: public Processor {
+protected:
+	const char *_chinese_number_end;
+	AsciiProcessor();
+	bool _can_process(LexToken *token)
+	{
+		return (token->get_attr() == LexToken::attr_unknow);
+	}
+	void _process(LexToken *token, std::vector<LexToken *> &out);
+public:
+	AsciiProcessor(IConfig *config);
+	~AsciiProcessor() {};
+};
+
+#endif // ASCII_PROCESSOR_HXX
