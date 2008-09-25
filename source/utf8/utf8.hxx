a618eddca5d89d0b26b2ee57226082e43842e7c2
detrox <detrox@22020200-94fc-11dd-9691-531050c25895>
2008-09-25 08:48:56 +0000

    made utf8::substr and utf8::first a little faster

--- source/utf8/utf8.hxx
+++ source/utf8/utf8.hxx
@@ -7,15 +7,13 @@
 
 class utf8 {
 protected:
-	static const char _map[];
+	static const char _map[], _map_ignore[];
 public:
 	static size_t first(const char *s, char *utf8ch)
 	{
-		size_t i = 1;
-
-		if (s == NULL) return 0; 
-		i = (_map[(unsigned char)*s] > 0)?_map[(unsigned char)*s]:1;
-		strncpy(utf8ch, s, i);
+		register size_t i;
+		i = _map_ignore[(unsigned char)*s];
+		memcpy(utf8ch, s, i);
 		utf8ch[i] = '\0';
 		return i;
 	}
@@ -101,31 +99,16 @@ public:
 
 	static size_t sub(char *t, const char *s, size_t start, size_t length)
 	{
-		size_t i, j, n;
+		register size_t i;
+		register char *begin = t;
 
-		if (s == NULL || t == NULL || length < 1) return 0;
-		for (i = 0, j = 0; s[i] && j < start;) {
-			if (_map[(unsigned char)s[i]] > 0) {
-				i += _map[(unsigned char)s[i]];
-				j++;
-			} else  {
-				++i;
-				continue;
-			}
+		for (i = 0; *s && i < start; s += _map_ignore[(unsigned char)*s], ++i) ;
+		while(length--) {
+			i = _map_ignore[(unsigned char)*s];
+			while(i--) *(t++) = *(s++);
 		}
-		for (j = 0; s[i] && length;) {
-			if (_map[(unsigned char)s[i]] > 0) {
-				length--;
-				n = _map[(unsigned char)s[i]];
-				while(n--) t[j++] = s[i++];
-			} else  {
-				++i;
-				continue;
-			}
-		}
-		t[j] = '\0';
-
-		return j;
+		*t = '\0';
+		return t - begin;
 	}
 };
 #endif
