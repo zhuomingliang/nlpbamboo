e684128b602a7cf5b379d7dec749c33b3a45b123
weibingzheng <weibingzheng@22020200-94fc-11dd-9691-531050c25895>
2008-09-26 01:48:34 +0000

    added original token attributes

--- source/cnlexizer/lex_token.hxx
+++ source/cnlexizer/lex_token.hxx
@@ -9,6 +9,7 @@
 class LexToken {
 protected:
 	char *_token;
+	char *_orig_token;
 	int _attr;
 	size_t _length;
 	size_t _bytes;
@@ -20,9 +21,9 @@ public:
 		attr_cword,
 		attr_punct,
 	};
-	LexToken():_token(NULL), _attr(attr_unknow) {}
+	LexToken():_token(NULL), _orig_token(NULL), _attr(attr_unknow), _length(0), _bytes(0) {}
 	LexToken(const char *s, int attr = attr_unknow)
-		:_attr(attr), _length(0), _bytes(0)
+		:_orig_token(NULL), _attr(attr), _length(0), _bytes(0)
 	{
 		_token = strdup(s);
 		_length = utf8::length(s);
@@ -34,9 +35,34 @@ public:
 			free(_token);
 			_token = NULL;
 		}
+		if(_orig_token) {
+			free(_orig_token);
+			_orig_token = NULL;
+		}
 	}
 	int get_attr() {return _attr;}
 	char *get_token() {return _token;}
+	char *get_orig_token() {
+		if(_orig_token) return _orig_token;
+		return _token;
+	}
+	void set_token(const char *s, int attr) {
+		set_token(s);
+		set_attr(attr);
+	}
+	void set_token(const char *s) {
+		if(!_orig_token) {
+			_orig_token = _token;
+		} else {
+			free(_token);
+		}
+		_token = strdup(s);
+		_length = utf8::length(s);
+		_bytes = strlen(s);
+	}
+	int set_attr(int attr) {
+		_attr = attr;
+	}
 	size_t get_length() {return _length;}
 	size_t get_bytes() {return _bytes;}
 };
